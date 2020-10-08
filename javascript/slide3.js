cff55a4e5e08f708514df9a1fd3870c24fc5a4bb
Chris Johns <chrisfotosmusic@gmail.com>
2020-10-08 14:40:11 -0400

    Now using CSS transitions

--- javascript/slide3.js
+++ javascript/slide3.js
@@ -1,3 +1,9 @@
-initSliders(
-    {pictures: ['./images/1.jpg','./images/2.jpg', './images/3.jpg', './images/4.jpg', './images/3.jpg', './images/4.jpg'], startingSize: 3, slideVelocity: 20}
+let slider = new Slider(
+    document.getElementsByClassName('sliderize')[0],
+    ['./images/1.jpg', './images/2.jpg', './images/3.jpg', './images/4.jpg'],
+    {
+    forwardButton: document.getElementsByClassName('for')[0],
+    backButton: document.getElementsByClassName('back')[0],
+    autoSlide: 4000
+    }
 )
